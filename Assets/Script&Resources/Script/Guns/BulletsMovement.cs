96715b7f01dcb43b17938357765b190212ef4789
dokozak <130685403+dokozak@users.noreply.github.com>
2025-01-23 09:16:29 +0100

    Idle animation and recharge animation added

--- Assets/Script&Resources/Script/Guns/BulletsMovement.cs
+++ Assets/Script&Resources/Script/Guns/BulletsMovement.cs
@@ -4,10 +4,23 @@ public class BulletsMovement : MonoBehaviour
 {
     public Vector3 movement;
 
-    int time;
+    public bool imShooted;
+    private float time;
+
+    private const int timeLife = 3; 
     // Update is called once per frame
     void Update()
     {
+        if (!imShooted)
+            return;
+       
+        if((time += Time.deltaTime) > timeLife)
+        {
+
+            return;
+        }
         
+
+
     }
 }
